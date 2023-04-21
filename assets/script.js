4731b7c0fdf53e31b249b22cdcd1aa89253572fe
marksalazar <105082689+marksalazr@users.noreply.github.com>
2023-04-20 22:54:55 -0700

    updated script.js

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,61 @@
-function GetInfo() {
+const newName = document.getElementById("cityInput");
+const cityName = document.getElementById("cityName");
+const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const imgSrc = "http://openweathermap.org/img/wn/";
 
-    var newName = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"+newName.value+"--";
-
-fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=ee0f9a588c4aa47825e2a42bb3f4672f')
-.then(response => response.json())
-.then(data => {
-
-    //Getting the min and max values for each day
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min).toFixed(1)+ "°";
-    
-    }
-
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max).toFixed(2) + "°";
-    }
-    //------------------------------------------------------------
-
-    //Getting Weather Icons
-     for(i = 0; i<5; i++){
-        document.getElementById("img" + (i+1)).src = "http://openweathermap.org/img/wn/"+
-        data.list[i].weather[0].icon
-        +".png";
-    }
-    
-
-
-})
-
-.catch(err => alert("Something Went Wrong: Try Checking Your Internet"))
+function getWeatherData(cityName) {
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast/?q=${cityName}&appid=ee0f9a588c4aa47825e2a42bb3f4672f&units=imperial`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = `Something went wrong: ${error.message}`;
+            document.body.appendChild(errorMessage);
+        });
 }
 
-function DefaultScreen(){
-    document.getElementById("cityInput").defaultValue = "Flagstaff";
-    GetInfo();
-}
+function setWeatherData(weatherData) {
+    for (let i = 0; i < 5; i++) {
+        const dayOfWeek = daysOfWeek[getDayOfWeek(i)];
+        const icon = imgSrc + weatherData.list[i].weather[0].icon + ".png";
+        const minTemp = Number(weatherData.list[i].main.temp_min).toFixed(1);
+        const maxTemp = Number(weatherData.list[i].main.temp_max).toFixed(2);
 
-
-//Getting and displaying the text for the upcoming five days of the week
-var d = new Date();
-var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
-
-//Function to get the correct integer for the index of the days array
-function CheckDay(day){
-    if(day + d.getDay() > 6){
-        return day + d.getDay() - 7;
-    }
-    else{
-        return day + d.getDay();
+        document.getElementById(`day${i+1}`).textContent = dayOfWeek;
+        document.getElementById(`img${i+1}`).src = icon;
+        document.getElementById(`day${i+1}Min`).textContent = `Min: ${minTemp}°`;
+        document.getElementById(`day${i+1}Max`).textContent = `Max: ${maxTemp}°`;
     }
 }
 
-    for(i = 0; i<5; i++){
-        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+function getDayOfWeek(day) {
+    const today = new Date();
+    let dayOfWeek = day + today.getDay();
+    if (dayOfWeek > 6) {
+        dayOfWeek -= 7;
     }
+    return dayOfWeek;
+}
+
+function getInfo() {
+    cityName.textContent = `--${newName.value}--`;
+    getWeatherData(newName.value)
+        .then(weatherData => setWeatherData(weatherData));
+}
+
+function defaultScreen() {
+    newName.defaultValue = "Flagstaff";
+    getInfo();
+}
+
+document.getElementById("cityInput").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        getInfo();
+    }
+});
+
+defaultScreen();
